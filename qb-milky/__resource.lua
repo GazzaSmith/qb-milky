1cb6e27d824fe700ebf02b4d8f1156edf8b23aee
GazzaSmith <92147748+GazzaSmith@users.noreply.github.com>
2021-12-28 17:16:44 +0000

    Add files via upload

---
+++ qb-milky/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+
+client_scripts {
+  
+  'client/cow_c.lua',
+  
+  'config.lua'
+}
+
+server_scripts {
+  
+  'server/cow_s.lua',
+ 
+  'config.lua'
+}
+
+
